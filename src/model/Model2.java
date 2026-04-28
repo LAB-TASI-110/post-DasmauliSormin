356139549bc06750f2f3f127530cdb4723817e95
DasmauliSormin <dasmauli06@gmail.com>
2026-04-28 16:07:35 +0700

    Implement kategori and stok in Model2 class
    Added fields and constructor to Model2 class.

--- src/model/Model2.java
+++ src/model/Model2.java
@@ -1,5 +1,19 @@
 package model;
 
 public class Model2 {
-	
+    private String kategori;
+    private int stok;
+
+    public Model2(String kategori, int stok) {
+        this.kategori = kategori.toUpperCase();
+        this.stok = stok;
+    }
+
+    public String getKategori() {
+        return kategori;
+    }
+
+    public int getStok() {
+        return stok;
+    }
 }
