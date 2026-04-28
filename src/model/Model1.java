f273c3c819a657ddb6f7e14fdc3d19d0059f0625
DasmauliSormin <dasmauli06@gmail.com>
2026-04-28 16:06:31 +0700

    Add fields and constructor to Model1 class

--- src/model/Model1.java
+++ src/model/Model1.java
@@ -1,5 +1,19 @@
 package model;
 
+
 public class Model1 {
-	
+    public String kode;
+    public String kota;
+    public double hargaPerKg;
+    public String kategori;
+
+    public double beratButet; // tambahan
+
+    public Model1(String kode, String kota, double hargaPerKg, String kategori, double beratButet) {
+        this.kode = kode;
+        this.kota = kota;
+        this.hargaPerKg = hargaPerKg;
+        this.kategori = kategori;
+        this.beratButet = beratButet;
+    }
 }
