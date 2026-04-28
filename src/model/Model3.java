76bf8f202f54bb6e9a75522895172dd43b1aa92c
DasmauliSormin <dasmauli06@gmail.com>
2026-04-28 16:08:17 +0700

    Update Model3.java

--- src/model/Model3.java
+++ src/model/Model3.java
@@ -1,5 +1,25 @@
+
 package model;
 
 public class Model3 {
-	
+    private String namaMenu;
+    private int harga;
+    private int jumlah;
+
+    public Model3(String namaMenu, int harga) {
+        this.namaMenu = namaMenu;
+        this.harga = harga;
+        this.jumlah = 0;
+    }
+
+    public Model3(String namaMenu, int harga, int jumlah) {
+        this.namaMenu = namaMenu;
+        this.harga = harga;
+        this.jumlah = jumlah;
+    }
+
+    public String getNamaMenu() { return namaMenu; }
+    public int getHarga() { return harga; }
+    public int getJumlah() { return jumlah; }
+    public int getSubtotal() { return harga * jumlah; }
 }
